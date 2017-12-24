3e57ace8e6d9c87c04f965aa0bc13ae9456e0a67
secret-dave <shivendra_chauhan@outlook.com>
2017-12-24 08:26:06 +0530

    Change UI framework to Semantic and use firebase to load menu

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,25 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyB6nvvS6_d0L5vFyK4sIoCVmPfQJ7XPB44",
-  authDomain: "hotandspicy-manipal.firebaseapp.com",
-  databaseURL: "https://hotandspicy-manipal.firebaseio.com",
-  projectId: "hotandspicy-manipal",
-  storageBucket: "hotandspicy-manipal.appspot.com",
-  messagingSenderId: "571495112460"
-};
-firebase.initializeApp(config);
 
 var CLIENT_ID = '571495112460-8ds913hb2lg3kgkj4a22aue2puppgu0o.apps.googleusercontent.com'
-//var firestore = firebase.firestore();
+
+const soup1 = document.querySelector("#soup1");
+const soup2 = document.querySelector("#soup2");
+const soup3 = document.querySelector("#soup3");
+const soup4 = document.querySelector("#soup4");
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+db.doc("menu/soups").get().then(function(doc) {
+  if (doc && doc.exists) {
+    const myData = doc.data();
+    soup1.innerText = myData.items[0].name;
+    soup2.innerText = myData.items[1].name;
+    soup3.innerText = myData.items[2].name;
+    soup4.innerText = myData.items[3].name;
+  }
+}).catch(function (error) {
+  console.log("Got an error: ", error);
+});
+
+
 //const docRef = firestore.doc("menu/shawarma");
 
 //This should appear in an accordion or something. 
